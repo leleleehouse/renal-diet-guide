6a2f12d4104da1cbe78c9b7ccefb1f81f635f26e
leleleehouse <dayom0124@naver.com>
2025-05-27 22:32:13 +0900

    json mod2

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "export", // 정적 사이트로 빌드 가능하게 추가
   experimental: {
-    optimizeCss: false, // LightningCSS 사용 안 하도록 설정 (권장)
+    optimizeCss: false, // lightningcss 비활성화 (중요)
   },
 };
 
